1f5747ecd8c7cf102f8c7c17b950bb0e6b1a92e9
Jeff Katzy <jeffreyerickatz@gmail.com>
2023-09-18 13:39:06 -0400

    add prefect codebase

---
+++ prefect-code/index.py
@@ -0,0 +1,20 @@
+import requests
+from prefect.server.schemas.schedules import IntervalSchedule
+from prefect import flow, task
+
+@task
+def find_receipts(name):
+    url = "https://data.texas.gov/resource/naix-2893.json"
+    response = requests.get(url, params = {'taxpayer_name': name})
+    return response.json()[:1]
+
+@flow
+def get_restaurants(url: str):
+    receipts = find_receipts(url)
+    return receipts
+
+if __name__ == "__main__":
+    get_restaurants("HONDURAS MAYA CAFE & BAR LLC")
+
+
+
